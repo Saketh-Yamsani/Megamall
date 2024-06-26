4703b5fd0f4c5bc4f7298a22acfa8b0450eb6f32
Saketh-Yamsani <5sakethyamsani@gmail.com>
2024-06-27 01:06:49 +0530

    commit

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -2,8 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from "../components/layout/layout";
+
 const Categories = () => {
   const categories = useCategory();
+
+  if (!categories || categories.length === 0) {
+    return (
+      <Layout title={"All Categories"}>
+        <div className="container" style={{ marginTop: "100px" }}>
+          <div className="row container">
+            <p>No categories available.</p>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout title={"All Categories"}>
       <div className="container" style={{ marginTop: "100px" }}>
