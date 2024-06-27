e9685cc516dd5fc1fcb7887ab7a69b9ca463b735
Saketh-Yamsani <5sakethyamsani@gmail.com>
2024-06-27 22:30:20 +0530

    Update Header.js

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🛒 GROCERY STORE
+              🛒 MegaMall
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />
